f429330c3bde1405380498f4aabf89da56ef6155
Joshua Green <aktungmak@gmail.com>
2026-01-21 07:51:39 +0100

    expose public API in __all__

---
+++ sparql2sql/__init__.py
@@ -0,0 +1,13 @@
+"""SPARQL to SQL translation using SQLAlchemy."""
+
+from sparql2sql.sparql2sql import (
+    Translator,
+    create_databricks_engine,
+    create_triples_table,
+)
+
+__all__ = [
+    "Translator",
+    "create_databricks_engine",
+    "create_triples_table",
+]
